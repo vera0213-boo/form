e9aab509bf341fcf27e9a83c88d3c4c748c731f7
vera0213-boo <vera0213911@gmail.com>
2025-03-31 20:35:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FormValidation from './component/FormValid'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='App'>
+     <FormValidation />
+         
+      </div>
+     
+    </>
+  )
+}
+
+export default App
